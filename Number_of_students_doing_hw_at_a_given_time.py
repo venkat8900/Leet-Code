69782cd989cf09e3d2e2893a5c671f98812de8a9
venkat8900 <41241851+venkat8900@users.noreply.github.com>
2021-03-26 13:22:07 +0530

    Add files via upload

---
+++ Number_of_students_doing_hw_at_a_given_time.py
@@ -0,0 +1,12 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count = 0
+        for i in range(len(startTime)):
+            if(startTime[i] <= queryTime and endTime[i] >= queryTime):
+                count += 1
+                i +=1
+            else: 
+                i += 1
+        
+        return count
+        
