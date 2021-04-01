5d81bb2800c6fbde2c00a8f6e95ec242313364c4
venkat8900 <41241851+venkat8900@users.noreply.github.com>
2021-04-01 19:20:17 +0530

    Add files via upload

---
+++ Airplane_seat_assignment_probability.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        
+        
+        if n == 1:
+            return 1
+        else:
+            return 1/2
+            
+        
