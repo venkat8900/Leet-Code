02255e914c97575b5ed0b09669c6a0fc279886cc
venkat8900 <41241851+venkat8900@users.noreply.github.com>
2021-06-08 16:08:24 +0530

    Add files via upload

---
+++ 589_N_ary_Tree_Preorder_traversal.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        out = []
+        
+        if root == None:
+            return out
+        
+        self.helper(root, out)
+        return out
+    
+    def helper(self, root, out):
+        out.append(root.val)
+        for child in root.children:
+            self.helper(child, out)
+        
